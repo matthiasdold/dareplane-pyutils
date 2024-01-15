249bf6de19c0e57a1fea8610751359e974f7eac6
matthiasdold <matthiasdold@gmx.net>
2024-01-15 14:10:47 +0100

    Colored log messages

--- src/dareplane_utils/logging/logger.py
+++ src/dareplane_utils/logging/logger.py
@@ -5,7 +5,8 @@ default_dareplane_config = {
     "version": 1,
     "formatters": {
         "dareplane_standard": {
-            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            "()": "colorlog.ColoredFormatter",
+            "format": "%(log_color)s%(asctime)s - %(name)s - %(levelname)s - %(reset)s %(white)s%(message)s",
         }
     },
     "handlers": {
@@ -24,16 +25,24 @@ default_dareplane_config = {
     },
 }
 
+# overwriting defaults
+colors = {"DEBUG": "cyan"}
+
 logging.config.dictConfig(default_dareplane_config)
 
 
 # have this as a simple wrapper to ensure the updated config is used
-def get_logger(name: str, add_console_handler: bool = False) -> logging.Logger:
+def get_logger(
+    name: str, add_console_handler: bool = False, colors: dict = colors
+) -> logging.Logger:
     logger = logging.getLogger(name)
     root_logger = logging.getLogger()
 
     if add_console_handler:
-        logger.addHandler(root_logger.handlers[0])
+        hdl = root_logger.handlers[0]
+        hdl.formatter.log_colors.update(colors)
+        logger.addHandler(hdl)
+
     logger.addHandler(root_logger.handlers[-1])  # add socket handler
 
     # do not propergate messages to root logger
