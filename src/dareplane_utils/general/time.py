e675b04c42ce7116a42edb2cf4caf888fff4dbd2
matthiasdold <matthias.dold@gmx.net>
2025-02-06 17:37:58 +0100

    added parametrization to the optimized sleep

--- src/dareplane_utils/general/time.py
+++ src/dareplane_utils/general/time.py
@@ -1,30 +1,45 @@
 import time
 
 
-# TODO: Think of parametrizing the thresholds
-def sleep_s(
-    s: float,
-):
+def sleep_s(s: float, partial_sleep_threshold: float = 0.0005, nsteps: int = 30):
     """
-
-    Sleep for s seconds.
+    Sleep for a specified duration with partial sleep optimization.
 
     Parameters
     ----------
     s : float
-        time in seconds to sleep
+        The total duration to sleep in seconds.
+    partial_sleep_threshold : float, optional
+        The threshold duration above which partial sleep optimization is applied, by default 0.0005.
+        I.e., only for durations `s` above the threshold, the optimization is applied.
+    nsteps : int, optional
+        The number of steps for partial sleep, by default 30. Empirical testing showed very good accuracy for 30.
+        If you want to optimize for CPU load, reduce to `nsteps` > 4.
 
     """
-
-    start = time.perf_counter_ns()
-    if s > 0.1:
-        # If not yet reached 90% of the sleep duration, sleep in 10% increments
-        # The 90% threshold is somewhat arbitrary but when testing intervals
-        # with 1 ms to 500ms this produced very accurate results with deviation
-        # less than 0.1% of the desired target value. On Mac M1 with python 3.11
-        while time.perf_counter_ns() - start < (s * 1e9 * 0.9):
-            time.sleep(s / 10)
+    start = time.perf_counter()
+    if s >= partial_sleep_threshold:
+        partial_sleep(s, start, nsteps)
 
     # Sleep for the remaining time
-    while time.perf_counter_ns() - start < s * 1e9:
+    full_speed(s, start)
+
+
+def partial_sleep(s: float, start: float, nsteps: int = 30):
+    """Sleep for 90% of `s` or up to 30ms to the end, whatever is longer"""
+
+    assert nsteps > 4, (
+        "Empirical tests showed that irrespective of the size of `s`, sleeping"
+        " seemes to maximally overshoot by a factor of 4. Keep nsteps larger "
+        "than 4 to be on the safe side."
+    )
+
+    s_max = max(s - 0.03, s * 0.9)
+
+    while time.perf_counter() - start < s_max:
+        time.sleep(s / nsteps)
+
+
+def full_speed(s: float, start: float):
+    while time.perf_counter() - start < s:
         pass
